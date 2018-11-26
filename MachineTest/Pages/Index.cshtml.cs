aeb03e2ccacdc66ceaced83ac6ff0b2d92e714bc
rozbehan <rozbehan@gmail.com>
2018-11-26 21:26:48 +0100

    First commit

--- MachineTest/Pages/Index.cshtml.cs
+++ MachineTest/Pages/Index.cshtml.cs
@@ -4,23 +4,31 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Formatting;
 
 namespace MachineTest.Pages
 {
     public class IndexModel : PageModel
     {
+        static string homeSecurityApiResponse;
         public string HomeSecurityStatus { get; private set; } = "DISARMED";
         public bool InputCodeVisible { get; private set; } = false;
 
-        public void OnGet()
-        {
-            
+        static HttpClient client = new HttpClient();
 
-        }
-        public IActionResult OnPost()
+
+        public static async Task<Uri> CreateProductAsync()
         {
-            InputCodeVisible = !InputCodeVisible;
-            return RedirectToPage("./Index");
+            var product = @"{'name':'mahdi'}";
+            HttpResponseMessage response = await client.PostAsJsonAsync(
+                @"https://gh21pxvwe5.execute-api.us-east-1.amazonaws.com/Prod", product);
+            response.EnsureSuccessStatusCode();
+
+            homeSecurityApiResponse = await response.Content.ReadAsStringAsync();
+
+            return response.Headers.Location;
         }
 
 
